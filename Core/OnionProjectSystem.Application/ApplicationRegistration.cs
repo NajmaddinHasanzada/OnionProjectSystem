7ee53a7d1f9c8bc87b9d7007d3cd837c3eaccf66
Najmaddin <najmaddinhasanzada@gmail.com>
2025-10-18 15:13:16 +0400

    Enhance rule validation and exception handling
    - Added `AddRulesFromAssemblyContaining` to dynamically register rule classes in `ApplicationRegistration.cs`.
    - Introduced `BaseExceptions` and `BaseRules` as foundational classes for custom exceptions and business rules.
    - Added `ProductRules` to enforce unique product titles and `ProductTitleMustNotBeSameException` for validation errors.
    - Updated `CreateProductCommandHandler` to integrate `ProductRules` for business rule enforcement.
    - Simplified `ExceptionMiddleware` by removing redundant `InnerException` logging.
    - Improved dependency injection and modularity for better maintainability and scalability.

--- Core/OnionProjectSystem.Application/ApplicationRegistration.cs
+++ Core/OnionProjectSystem.Application/ApplicationRegistration.cs
@@ -4,6 +4,8 @@ using System.Reflection;
 using FluentValidation;
 using MediatR;
 using OnionProjectSystem.Application.Beheviors;
+using OnionProjectSystem.Application.Features.Products.Rules;
+using OnionProjectSystem.Application.Bases;
 namespace OnionProjectSystem.Application
 {
     public static class ApplicationRegistration
@@ -14,6 +16,8 @@ namespace OnionProjectSystem.Application
 
             services.AddTransient<ExceptionMiddleware>();
 
+            services.AddRulesFromAssemblyContaining(assembly, typeof(BaseRules));
+
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
 
             services.AddValidatorsFromAssembly(assembly);
@@ -21,6 +25,19 @@ namespace OnionProjectSystem.Application
             ValidatorOptions.Global.LanguageManager.Culture = new System.Globalization.CultureInfo("az");
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehevior<,>));
+        }
+
+       private static IServiceCollection AddRulesFromAssemblyContaining(this IServiceCollection services, Assembly assembly, Type type)
+        {
+            var types = assembly.GetTypes()
+                .Where(t => t.IsSubclassOf(type) && type!=t).ToList();
+
+            foreach (var t in types)
+            {
+                services.AddTransient(t);
+            }
+
+            return services;
         }
     }
 }
