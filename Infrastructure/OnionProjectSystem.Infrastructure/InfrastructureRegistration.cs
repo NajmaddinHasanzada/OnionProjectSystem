abaebbfceacb623ab5cde51185f7e02366879275
Najmaddin <najmaddinhasanzada@gmail.com>
2025-10-19 20:21:13 +0400

    Added JWT Configurations and Token Service

--- Infrastructure/OnionProjectSystem.Infrastructure/InfrastructureRegistration.cs
+++ Infrastructure/OnionProjectSystem.Infrastructure/InfrastructureRegistration.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using OnionProjectSystem.Application.Interfaces.Tokens;
 using OnionProjectSystem.Infrastructure.Tokens;
 using System;
 using System.Collections.Generic;
@@ -14,6 +17,26 @@ namespace OnionProjectSystem.Infrastructure
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+            services.AddTransient<ITokenService, TokenService>();
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, option =>
+            {
+                option.SaveToken = true;
+                option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                    ValidateLifetime = false,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
     }
 }
